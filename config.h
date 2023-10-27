3044ca30a90896888a13453aea488c9307bb9bc5
Hill <hillyu@live.com>
2023-10-27 15:42:31 +0800

    update config

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER "︱"
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -13,17 +13,19 @@
 #define LEADING_DELIMITER 0
 
 // Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 0
+#define TRAILING_DELIMITER 1
 
 // Define blocks for the status feed as X(cmd, interval, signal).
-#define BLOCKS(X)         \
-    X("sb-mail", 600, 1)  \
-    X("sb-music", 0, 2)   \
-    X("sb-disk", 1800, 3) \
-    X("sb-memory", 10, 4) \
-    X("sb-loadavg", 5, 5) \
-    X("sb-mic", 0, 6)     \
-    X("sb-record", 0, 7)  \
-    X("sb-volume", 0, 8)  \
-    X("sb-battery", 5, 9) \
-    X("sb-date", 1, 10)
+#define BLOCKS(X)                                                           \
+    X("~/.config/scripts/mpd 2>/dev/null|head -n1",0,8)                 \
+    X("~/.config/scripts/iface 2>/dev/null|head -n1",13,17)            \
+    X("~/.config/scripts/bandwidth 2>/dev/null|head -n1",5,18)        \
+    X("~/.config/scripts/load_average 2>/dev/null|head -n1 ",11,20)    \
+    X("~/.config/scripts/temperature.i3 2>/dev/null|head -n1 ",3,21)    \
+    X("~/.config/scripts/cpu 2>/dev/null",5,24)                    \
+    X("~/.config/scripts/battery 2>/dev/null|head -n1 ",31,10)        \
+    X("~/.config/scripts/udiskie 2>/dev/null|head -n1 ",5,25)          \
+    X("~/.config/scripts/volume-pulseaudio 2>/dev/null|head -n1 ", 0,12)\
+    X("~/.config/scripts/i3weather 2>/dev/null|head -n1",100,22)    \
+    X("~/.config/scripts/clock 2>/dev/null|head -n1",10, 23)            \
+    X("~/.config/scripts/bluetooth.i3 2>/dev/null|head -n1 ",0, 5)
